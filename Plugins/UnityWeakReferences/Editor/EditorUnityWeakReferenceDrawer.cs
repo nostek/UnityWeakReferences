c3a13deb29b65dfbfee8b32f4ba795371ef61739
Simon Rodriguez <simon.rodriguez@starstable.com>
2018-03-22 16:27:57 +0100

    Support for drawer in nested classes

--- Plugins/UnityWeakReferences/Editor/EditorUnityWeakReferenceDrawer.cs
+++ Plugins/UnityWeakReferences/Editor/EditorUnityWeakReferenceDrawer.cs
@@ -6,7 +6,7 @@ public class EditorUnityWeakReferenceDrawer : PropertyDrawer
 {
 	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 	{
-		var obj = fieldInfo.GetValue(property.serializedObject.targetObject);
+		var obj = PropertyPath(property, fieldInfo);
 
 		var weakType = default(System.Type);
 
@@ -24,5 +24,27 @@ public class EditorUnityWeakReferenceDrawer : PropertyDrawer
 	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 	{
 		return EditorGUIUtility.singleLineHeight;
+	}
+
+	static System.Object PropertyPath(SerializedProperty property, System.Reflection.FieldInfo fieldInfo)
+	{
+		System.Object prop = property.serializedObject.targetObject;
+
+		string[] path = property.propertyPath.Split('.');
+
+		for (int i = 0; i < path.Length; i++)
+		{
+			string node = path[i];
+
+			if (i == path.Length - 2 && node == "Array" && path[i + 1].StartsWith("data["))
+				break;
+
+			prop = prop
+				.GetType()
+				.GetField(node, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+				.GetValue(prop);
+		}
+
+		return prop;
 	}
 }
