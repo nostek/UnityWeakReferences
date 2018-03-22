5c579d70a7818d9a9289b493e5d1ad06789ed3bb
Simon Rodriguez <simon.rodriguez@starstable.com>
2018-03-22 15:27:11 +0100

    Support for List

--- Plugins/UnityWeakReferences/Editor/EditorUnityWeakReferenceDrawer.cs
+++ Plugins/UnityWeakReferences/Editor/EditorUnityWeakReferenceDrawer.cs
@@ -6,10 +6,16 @@ public class EditorUnityWeakReferenceDrawer : PropertyDrawer
 {
 	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 	{
-		System.Type weakType =
-			(fieldInfo.FieldType.IsArray)
-			? ((UnityWeakReference[])fieldInfo.GetValue(property.serializedObject.targetObject))[0].GetWeakType()
-			: ((UnityWeakReference)fieldInfo.GetValue(property.serializedObject.targetObject)).GetWeakType();
+		var obj = fieldInfo.GetValue(property.serializedObject.targetObject);
+
+		var weakType = default(System.Type);
+
+		if (obj is System.Collections.IList)
+			weakType = (((obj as System.Collections.IList)[0]) as UnityWeakReference).GetWeakType();
+		else if (obj.GetType().IsArray)
+			weakType = ((UnityWeakReference[])obj)[0].GetWeakType();
+		else
+			weakType = ((UnityWeakReference)obj).GetWeakType();
 
 		var prefab = property.FindPropertyRelative("reference");
 		prefab.objectReferenceValue = EditorGUI.ObjectField(position, label, prefab.objectReferenceValue, weakType, false);
