a74b3b7734fc52c56def69b1e364d9029192cb0f
Simon Rodriguez <simon.rodriguez@starstable.com>
2018-03-22 13:51:43 +0100

    Support for Arrays

--- Plugins/UnityWeakReferences/Editor/EditorUnityWeakReferenceDrawer.cs
+++ Plugins/UnityWeakReferences/Editor/EditorUnityWeakReferenceDrawer.cs
@@ -6,10 +6,13 @@ public class EditorUnityWeakReferenceDrawer : PropertyDrawer
 {
 	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 	{
-		UnityWeakReference obj = (UnityWeakReference)fieldInfo.GetValue(property.serializedObject.targetObject);
+		System.Type weakType =
+			(fieldInfo.FieldType.IsArray)
+			? ((UnityWeakReference[])fieldInfo.GetValue(property.serializedObject.targetObject))[0].GetWeakType()
+			: ((UnityWeakReference)fieldInfo.GetValue(property.serializedObject.targetObject)).GetWeakType();
 
 		var prefab = property.FindPropertyRelative("reference");
-		prefab.objectReferenceValue = EditorGUI.ObjectField(position, label, prefab.objectReferenceValue, obj.GetWeakType(), false);
+		prefab.objectReferenceValue = EditorGUI.ObjectField(position, label, prefab.objectReferenceValue, weakType, false);
 	}
 
 	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
